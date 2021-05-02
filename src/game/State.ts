7dd1f942756e59d394dd3d292679fd1df2098c18
Pekka Siiriäinen <pekka.siiriainen@wundermanthompson.com>
2021-05-02 11:58:36 +0300

    Removed commented out code.

--- src/game/State.ts
+++ src/game/State.ts
@@ -61,39 +61,6 @@ const State = (state: GameState, keyIsDown: Function, canvas: HTMLCanvasElement)
       }
       return
     }
-
-
-    // const directionX = Math.round(state.player.velocities.x) > 0 ? 'fromRight' : 'fromLeft'
-    // const directionY = Math.round(state.player.velocities.y) > 0 ? 'fromAbove' : 'fromBelow'
-
-    // console.log(directionX, directionY)
-
-    // if (state.player.location.x <= frame.x) {
-    //   state.player.location.x = 0
-    //   state.player.velocities.x = (state.player.velocities.x * -1) + (state.player.velocities.x / playerBouncinessDivider)
-    // }
-    // else if (state.player.location.x + state.player.size >= canvas.width) {
-    //   state.player.location.x = canvas.width - state.player.size
-    //   state.player.velocities.x = (state.player.velocities.x * -1) + (state.player.velocities.x / playerBouncinessDivider)
-    // }
-
-    // if (state.player.location.y <= 0) {
-    //   state.player.location.y = 0
-    //   state.player.velocities.y = (state.player.velocities.y * -1) + (state.player.velocities.y / playerBouncinessDivider)
-    // }
-    // else if (state.player.location.y + state.player.size >= canvas.height) {
-    //   state.player.location.y = canvas.height - state.player.size
-    //   state.player.velocities.y = (state.player.velocities.y * -1) + (state.player.velocities.y / playerBouncinessDivider)
-    // }
-
-    // if (state.player.location.x <= frame.location.x + frame.size) {
-    //   state.player.location.x = frame.location.x + frame.size
-    //   state.player.velocities.x = (state.player.velocities.x * -1) + (state.player.velocities.x / playerBouncinessDivider)
-    // }
-    // else if (state.player.location.x + state.player.size >= canvas.width) {
-    //   state.player.location.x = canvas.width - state.player.size
-    //   state.player.velocities.x = (state.player.velocities.x * -1) + (state.player.velocities.x / playerBouncinessDivider)
-    // }
   }
 
   const moveBoxes = (): void => {
